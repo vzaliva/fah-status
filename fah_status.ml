1205e100f89817e5215d25e89d630604936a7c32
Vadim Zaliva <lord@crocodile.org>
2020-03-21 19:43:36 -0700

    using FAH client

--- fah_status.ml
+++ fah_status.ml
@@ -1,2 +1,7 @@
-
-let () = print_endline "Hello world!"
+let () =
+  let addr =
+    `Socket(`Sock_inet_byname(Unix.SOCK_STREAM, "localhost", 36330),
+            Uq_client.default_connect_options) in
+  let c = new Fah.connect addr 60.0 in
+  print_endline (string_of_bool (c # is_configured ())) ;
+  print_endline "Hello world!"
