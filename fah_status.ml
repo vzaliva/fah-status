d7fc0d24e49ecaec4b359c46c20b6d7172adfc6b
Vadim Zaliva <lord@crocodile.org>
2020-05-06 20:45:28 -0700

    re-connection logic

--- fah_status.ml
+++ fah_status.ml
@@ -5,7 +5,12 @@ open LTerm_key
 
 let jempty = Yojson.Basic.from_string "[]"
 
-let state = ref jempty
+type cstate = 
+  | Connecting
+  | NotConfigured
+  | Connected of Yojson.Basic.t
+
+let state = ref Connecting
 
 let draw ui matrix state =
   let size = LTerm_ui.size ui in
@@ -13,10 +18,13 @@ let draw ui matrix state =
   LTerm_draw.clear ctx;
   let ts = Float.to_string (Unix.time ()) in
   LTerm_draw.draw_styled ctx 0 0 (eval [B_fg yellow ; S ts ; E_fg]);
-  if Yojson.Basic.equal state jempty then
-    LTerm_draw.draw_styled ctx 1 0 (eval [B_fg red   ; S"Disconnected" ; E_fg])
-  else
-    LTerm_draw.draw_styled ctx 1 0 (eval [B_fg green ; S"Connected"    ; E_fg])
+  match state with
+  | Connecting ->
+     LTerm_draw.draw_styled ctx 1 0 (eval [B_fg red   ; S"Disconnected" ; E_fg])
+  | NotConfigured ->
+     LTerm_draw.draw_styled ctx 1 0 (eval [B_fg red   ; S"Not Configured" ; E_fg])
+  | Connected _ ->
+     LTerm_draw.draw_styled ctx 1 0 (eval [B_fg green ; S"Connected"    ; E_fg])
 
 (* in seconds *)
 let update_period = 1.0
@@ -25,35 +33,55 @@ type event_or_tick = LEvent of LTerm_event.t | LTick
 let wait_for_event ui = LTerm_ui.wait ui >>= fun x -> return (LEvent x)
 let wait_for_tick () = Lwt_unix.sleep update_period >>= fun () -> return (LTick)
 
-let rec loop c ui event_thread tick_thread =
+let rec loop host port c ui event_thread tick_thread =
   Lwt.choose [ event_thread; tick_thread ] >>= fun e ->
   LTerm_ui.draw ui;
   match e with
   | LEvent (LTerm_event.Key {code = Escape; _}) ->
-     return ()
+     begin
+       (* TODO: maybe close connection here *)
+       return ()
+     end
   | LTick ->
-     state :=
-       (try
-          (if (c # is_configured ()) then
-             c # info ()
-           else
-             jempty)
-        with
-        | _ -> jempty)
-       ;
-     loop c ui event_thread (wait_for_tick ())
+     begin
+       match c with
+       | None ->
+          begin
+            try
+              (* TODO: check for socket leak *)
+              let addr =
+                `Socket(`Sock_inet_byname(Unix.SOCK_STREAM, host, port),
+                        Uq_client.default_connect_options) in
+              let c = new Fah.connect addr 60.0 in
+              state := Connecting ;
+              loop host port (Some c) ui event_thread (wait_for_tick ())
+            with
+            | _ -> loop host port None ui event_thread (wait_for_tick ())
+          end
+       | Some c ->
+          begin
+            try
+              if (c # is_configured ()) then
+                (state := Connected (c # info ());
+                 loop host port (Some c) ui event_thread (wait_for_tick ()))
+              else
+                (state := NotConfigured;
+                 loop host port (Some c) ui event_thread (wait_for_tick ()))
+            with
+            | _ ->
+               c # close () ;
+               state := Connecting;
+               loop host port None ui event_thread (wait_for_tick ())
+          end
+     end
   | _ ->
-     loop c ui (wait_for_event ui) tick_thread
+     loop host port c ui (wait_for_event ui) tick_thread
 
 let main () =
-  let addr =
-    `Socket(`Sock_inet_byname(Unix.SOCK_STREAM, "localhost", 36330),
-            Uq_client.default_connect_options) in
-  let c = new Fah.connect addr 60.0 in
   let term = Lwt_main.run (Lazy.force LTerm.stdout) in
   let ui = Lwt_main.run (LTerm_ui.create term
                            (fun matrix size -> draw matrix size !state)) in
-  (loop c ui (wait_for_event ui) (wait_for_tick ()))
+  (loop "localhost" 36330 None ui (wait_for_event ui) (wait_for_tick ()))
     [%lwt.finally LTerm_ui.quit ui]
 
 (* debug dump *)
@@ -79,8 +107,9 @@ let dump () =
     print_endline "=======================";
   done
 
-let () = dump ()
+
+let () = Lwt_main.run (main ())
 
 (*
-let () = Lwt_main.run (main ())
+let () = dump ()
  *)
